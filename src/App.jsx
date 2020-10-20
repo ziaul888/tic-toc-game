cc48211e726a2fabcd98f503688e1832acf0e173
unknown <ziaul935@gmail.com>
2020-10-21 01:31:37 +0600

    app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Game from './compoonents/Game'
+
+const App =()=>{
+    return(
+        <div className='App'>
+          <Game/>
+        </div>
+    )
+
+}
+export default App
